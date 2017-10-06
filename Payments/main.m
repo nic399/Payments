c172ac2932170b576fb8a19ba7c4be65cae38bcd
Nicholas Fung <nbfung@sfu.ca>
2017-10-06 10:43:30 -0700

    Add InputHandler

--- Payments/main.m
+++ Payments/main.m
@@ -8,18 +8,20 @@
 
 #import <Foundation/Foundation.h>
 #import "general_defines.h"
+#import "InputCollector.h"
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
+        InputCollector *myInputCollector = [[InputCollector alloc] init];
+        
+        double price = ((double)arc4random_uniform(900))+101;
+        NSLog(@"Thank you for shopping at LiteShackLabs.ca");
+        NSLog(@"Your total today is $%.2f", price);
+        NSLog(@"Please select your payment method:");
+        NSLog(@"1: Paypal, 2: Stripe, 3: Amazon");
+        NSString *userInput = [myInputCollector inputForPrompt:@">"];
+        NSLog(@"%@",userInput);
 
-        
-        double price = ((double)arc4random_uniform(100000))/100.00;
-        
-        NSLog(@"Cost: $%.2f", price);
-        
-        
-        
-        
     }
     return 0;
 }
